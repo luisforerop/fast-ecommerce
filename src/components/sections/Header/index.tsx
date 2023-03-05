a2e52f0b6c6c418a5b5b87e72278f72661275ea1
Luis <Luis@LAPTOP-0EFHT9GS>
2023-03-04 23:56:00 -0500

    feat: :sparkles: Implement Header component.

---
+++ src/components/sections/Header/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+export const Header = () => {
+  return <header className={styles.container}>FAST ECOMMERCE</header>
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { Header } from '@/components/sections'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Header />
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
